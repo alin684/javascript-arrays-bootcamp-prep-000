710a953df1fd64b8de9af78704fca57fe213bd0c
Abraham Lin <al684@cornell.edu>
2017-12-05 07:49:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = ['snickers','hundred grand','kitkat','skittles'];
 
 function addElementToBeginningOfArray(array1, element1) {
   var newarray = [element1,...array1];
-  return newarry;
+  return newarray;
 }
 function destructivelyAddElementToBeginningOfArray(array2, element2) {
   [array2,...element];
